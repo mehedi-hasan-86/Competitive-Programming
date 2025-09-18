14b2a43cf0319d392e933a11d6723a0aed41cbeb
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-09-18 23:36:16 +0600

    Add LeetCode solution: Valid Palindrome

---
+++ LeetCode/Valid Palindrome.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int n = s.size();
+        string res = "";
+        for(int i=0; i<n; i++){
+            if(isalpha(s[i])){
+                s[i] = tolower(s[i]);
+                res.push_back(s[i]);
+            }
+        }
+        string rev = res;
+        reverse(rev.begin(), rev.end());
+
+        return rev == res;
+        
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    Solution sol;
+
+    string s;
+    getline(cin , s);
+
+    cout << sol.isPalindrome(s) << endl;
+
+    return 0;
+}
