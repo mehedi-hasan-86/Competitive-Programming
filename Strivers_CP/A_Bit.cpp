f99fc43f45c279ac12b7480ceabb1aa4d6a5884f
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-09-19 22:12:05 +0600

    Add Strivers_CP folder

---
+++ Strivers_CP/A_Bit.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >>n;
+
+    int res = 0;
+    while(n--){
+        string s;
+        cin >> s;
+
+        if(s == "++X" ||  s== "X++"){
+          res++;
+        }else{
+            res--;
+        }
+    }
+    cout << res << endl;
+
+    return 0;
+}
