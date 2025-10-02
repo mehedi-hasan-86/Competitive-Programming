5f622746374cf19cffa3a873539dd4cb081966ba
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-10-02 18:39:47 +0600

    Day 12

---
+++ Strivers_A2Z_DSA/Search_Insert_Position.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int it = lower_bound(nums.begin(), nums.end(),target) - nums.begin();
+
+        return it;
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    Solution sol;
+
+    int n,tg;
+    cin >> n >> tg;
+
+    vi a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    cout << sol.searchInsert(a , tg) << endl;
+
+    return 0;
+}
