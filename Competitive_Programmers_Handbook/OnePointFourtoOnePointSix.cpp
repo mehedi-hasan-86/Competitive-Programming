1cea220db41c7f61a34922059acecf78f61c8ac4
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2025-11-22 22:02:53 +0600

    TwoPointOneTwoPointThree.cpp

---
+++ Competitive_Programmers_Handbook/OnePointFourtoOnePointSix.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+typedef long long ll;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    long long a = 123456789;
+    long long b = 987654321;
+    cout << a*b << endl;
+
+    ll m = 123456789;
+    ll n = 987654321;
+    cout << m*n << endl;
+
+
+
+    return 0;
+}
---
+++ Competitive_Programmers_Handbook/TwoPointOneTwoPointThree.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        //code
+        // O(n)
+    }
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; i++){
+            //code
+            //O(n*n)
+        }
+    }
+
+    for(int i=1; i<=3*n; i++){
+        //code
+        //O(n)
+    }
+    for(int i=1; i<=n+5; i++){
+        //code
+        //O(n)
+    }
+    for(int i=1; i<=n; i+=2){
+        //code
+        //O(n)
+    }
+
+    int m;
+    cin >> m;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            //code
+            // O(n*m)
+        }
+    }
+
+
+
+
+    return 0;
+}
