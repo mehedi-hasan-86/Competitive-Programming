1974f84c6d71de64c4037194330ce24854f90010
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-01-07 19:58:30 +0600

    Basic_Input_Output.cpp

---
+++ Strivers_A2Z_DSA/Basic_Input_Output.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+    
+
+    // int day;
+    // cin >> day;
+
+    // switch(day){
+    //     case 1:  cout <<"Friday"; break;
+    //     case 2: cout <<"Saturday";  break;
+    //     case 3: cout << "Sunday"; break;
+    //     case 4: cout << "Monday"; break;
+    //     case 5: cout << "Tuesday"; break;
+    //     case 6: cout <<"Wednesday"; break;
+    //     case 7: cout << "Thursday"; break;
+    //     default : cout << "Invalid"; break;
+    // }
+    // cout << "Check";
+
+
+
+
+
+    // std :: cout << "Hey, Mehedi" << "\n";
+    // std :: cout << "Hey, Mehedi" << std::endl;
+    // std :: cout << "Hey, Mehedi";
+
+    // int x,y;
+    // cin >> x  >> y;
+    // cout << x  << " " <<  y <<endl;
+    
+    //int
+    // int x = 10;
+    
+    // //long
+    // long x = 15;
+
+    // long long x = 1500000000;
+    // float x = 5.6;
+    // double y = 3.5;
+
+    // //String
+    // string s;
+    // cin >> s;
+    // cout << s << endl;
+
+    // // input string with space
+    // string str;
+    // getline(cin, str);
+    // cout << str;
+
+    // // char 
+    // char c ;
+    // cin >> c;
+
+    //  int marks;
+    //  cin >> marks;
+    //  if(marks<25){
+    //     cout <<"F";
+    //  }else if( marks <=44){
+    //     cout << "E";
+    //  }else if(marks <=49){
+    //     cout << "D";
+    //  }else if(marks <=59){
+    //     cout << "C";
+    //  }else if(marks <=79){
+    //     cout << "B";
+    //  }else  if( marks <=100){
+    //     cout << "A";
+    //  }
+
+    // int age;
+    // cin >> age;
+    // if(age<18){
+    //     cout <<"Not elidible for job";
+    // }else if(age<=54){
+    //     cout <<"eligible for job";
+    // }
+
+}
