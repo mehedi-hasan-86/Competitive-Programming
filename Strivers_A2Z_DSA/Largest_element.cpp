07a295b257f4fa1d0a38c7c07bd9894d11888229
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-01-27 19:10:44 +0600

    Largest_element.cpp

---
+++ Strivers_A2Z_DSA/Largest_element.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vi a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    int largest = a[0];
+    for(int i=0; i<n; i++){
+        if(a[i]>largest){
+            largest = a[i];
+        }
+    }
+    cout << largest << endl;
+
+    return 0;
+}
