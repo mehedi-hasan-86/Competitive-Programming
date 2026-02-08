99477764513f66cb8207cfab3b1aef8fa5837c6b
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-02-08 18:25:53 +0600

    A_Lawn_Mower.cpp

---
+++ Codeforces/A_Lawn_Mower.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        lli n,w;
+        cin >> n >> w;
+
+         cout << n-n/w << endl;
+    }
+
+    return 0;
+}
