95ce94058196d0938fb6c5699409eca7da1b2d66
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-02-08 21:38:29 +0600

    Longest_subarray_with_sum_K.cpp

---
+++ Strivers_A2Z_DSA/Longest_subarray_with_sum_K.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vi a(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    int mxLen = 0;
+    for(int i=0; i<n; i++){
+        int sum = 0;
+        for(int j=i; j<n; j++){
+            sum +=a[j];
+            if(sum ==0){
+                mxLen = max(mxLen, j-i+1);
+            }
+        }
+    }
+    cout << mxLen << endl;
+
+    return 0;
+}
