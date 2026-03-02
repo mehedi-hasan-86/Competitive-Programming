c8f4a2107cfe1045afe37d08a18ce7467f0a5b54
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-03-02 07:08:09 +0600

    A_Perfect_Root.cpp

---
+++ Codeforces/A_Perfect_Root.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        
+        for(int i=1; i<=n; i++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
