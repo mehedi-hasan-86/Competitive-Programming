9c57d5b41e682836f0f5d7ed575fdd2636bf7bb3
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-03-07 15:48:27 +0600

    A_Word_Capitalization.cpp

---
+++ Strivers_CP/A_Word_Capitalization.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    if(islower(s[0])){
+        s[0] = s[0]-32;
+    }
+    cout << s << endl;
+
+    return 0;
+}
