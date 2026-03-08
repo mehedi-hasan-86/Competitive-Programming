6a0ff1080d5011ee50c6adcda57608c8880e00be
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-03-08 07:09:29 +0600

    B_Move_and_Turn.cpp

---
+++ CP_31/B_Move_and_Turn.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    lli n;
+    cin >> n;
+
+    lli k = n/2;
+
+    if(n%2==0) cout << (k+1)*(k+1);
+    else cout << 2*(k+1)*(k+2);
+
+    return 0;
+}
