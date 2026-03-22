289ddba0141ff219a697f3e2d065d352662d2b23
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-03-22 15:58:56 +0600

    Trailing_Zeros.cpp

---
+++ CSES_Problemset/Trailing_Zeros.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int cnt  = 0;
+    while(n>0){
+        n /=5;
+        cnt +=n;
+    }
+    cout << cnt << endl;
+
+    return 0;
+}
