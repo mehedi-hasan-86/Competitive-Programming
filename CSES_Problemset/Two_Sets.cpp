c525fa5d960aa23ec448d168462cc002b084cc40
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-03-25 12:07:00 +0600

    Two_Sets.cpp

--- CSES_Problemset/Two_Sets.cpp
+++ CSES_Problemset/Two_Sets.cpp
@@ -11,43 +11,37 @@ const int MAX = 1e6 + 5;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
+
     lli n;
     cin >> n;
 
-    lli total_sum = n*(n+1)/2;
-    if(total_sum%2==0){
-        cout <<"YES" << endl;
-        
-        int vis[n+1] = {0};
-        lli  target_sum = 0;
-        lli val = 0;
-        
-        for(lli i=n; i>=1; i--){
-            target_sum += i;
-            if(target_sum < total_sum/2){
-            vis[i] = 1;
-            val = target_sum;
-           }
-        }
-        vis[total_sum/2 - val]=1;
-        int cnt1 = count(vis, vis+n+1, 1);
-        cout << cnt1 << endl;
-        for(int i=1; i<=n; i++){
-            if(vis[i]==1){
-                cout << i << " ";
-            }
-        }
-        cout << endl;
-        cout << count(vis, vis+n+1,0)-1 << endl;
-        for(int i=1; i<=n; i++){
-            if(vis[i]==0){
-                cout << i << " ";
-            }
-        }
-        cout << endl;
-    }else{
-        cout << "NO" << endl;
+    lli ans = n*(n+1)/2;
+    if(ans%2 !=0){
+        cout <<"NO" << endl;
+        return 0;
     }
+
+    cout <<"YES" << endl;
+    lli sum = ans/2;
+    vl a,b;
+    for(int i=n; i>=1; i--){
+        if(sum >=i){
+            sum -=i;
+            a.push_back(i);
+        }else{
+            b.push_back(i);
+        }
+    }
+    cout << a.size() <<endl;
+    for(auto x : a){
+        cout << x << " ";
+    }
+    cout << endl;
+    cout << b.size() << endl;
+    for(auto x : b){
+        cout << x << " ";
+    }
+    cout << endl;
 
     return 0;
 }
