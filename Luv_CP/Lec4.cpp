6647f95c845edf6e7bd35c653bd69e6df3b926f0
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-10 13:22:13 +0600

    Lec4.cpp

---
+++ Luv_CP/Lec4.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cout <<"Hello" << endl;
+    cout << sqrt(16) << endl;
+    cout <<"My name is Luv" << endl;
+
+    int a = 2;
+    // int b = 3;
+    // cout << a + b << endl;
+
+    char c = 'a';
+    cout << c << endl;
+    double b = 3.5; 
+    cout << b << endl;
+
+    bool d = 0; // false
+    bool e = 1 ;// true
+    bool f = -1; // true
+
+    int g = (20%6) ; // g = 2
+    int h = 5;
+    h = h+1; // 6
+    cout << h << endl; // 6
+    cout << h++ << endl; // 6
+    cout << ++h << endl; // 8
+    h = h-1; // 7
+    cout << h-- << endl; // 7
+    cout << --h << endl; // 5
+
+    char i = 'a';
+    char j  = 'k';
+    cout << j-i << endl;
+
+    int l;
+    double m;
+    char n;
+    cin >> l >> m >> n;
+    cout << l << " " << m << " " << n << endl;
+
+    int o,p,q;
+    char s;
+    cin >> s >> p;
+    cout << s << " " << p << endl;
+
+
+
+
+
+    return 0;
+}
