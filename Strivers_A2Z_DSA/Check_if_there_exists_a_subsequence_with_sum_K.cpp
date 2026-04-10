4a437a48ca6fc607fd4eef6f6f527f63bcdcd521
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-10 12:03:08 +0600

    Check_if_there_exists_a_subsequence_with_sum_K.cpp

---
+++ Strivers_A2Z_DSA/Check_if_there_exists_a_subsequence_with_sum_K.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+bool func(int ind, int sum, vi &nums){
+    if(ind == nums.size()){
+        return sum == 0;
+    }
+    return func(ind+1, sum-nums[ind], nums) | func(ind+1,sum, nums);
+}
+
+bool checkSubSequenceSum(vi nums, int target){
+    return func(0, target, nums);
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    vi nums = { 1,2,3,4};
+    int target = 5;
+    cout << checkSubSequenceSum(nums, target) << endl;
+
+
+
+    return 0;
+}
