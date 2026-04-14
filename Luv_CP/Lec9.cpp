5ce7332cfc7de0835eef6d8fd7ebeb0111a6142c
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-14 10:11:21 +0600

    Lec9.cpp

---
+++ Luv_CP/Lec9.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+const int n = 1e7;
+int a[n];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // int a[10];
+    // a[9] = 10;
+    // cout << a[9] << endl;
+
+    // int n;
+    // cin >> n;
+    // int a[n];
+    // for(int i=0; i<n; i++){
+    //     cin >> a[i];
+    // }
+
+    // lli sum = 0;
+    // for(int i=0; i<n; i++){
+    //     sum +=a[i];
+    // }
+    // cout << sum << endl;
+
+    // int n,m;
+    // cin >> n>>m;
+
+    // int a[n][m];
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<m; j++){
+    //         cin >> a[i][j];
+    //     }
+    // }
+    // for(int i=0; i<n; i++){
+    //     for(int j=0; j<m; j++){
+    //         cout << a[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // int n = 1e7;
+    // int a[n];
+    a[n-1] = 7;
+    cout << a[n-1] << endl;
+
+
+    return 0;
+}
