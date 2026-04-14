883668cceec98aa59dce8bdb0ab47b6ccad5b33b
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-14 10:14:58 +0600

    Lec10.cpp

---
+++ Luv_CP/Lec10.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // Just only where we declare and what is the best
+    /*
+    1. if we declare array in local that is worst case  because
+     only 1 million can store 
+    2. if we declare array in globally that is like 
+    static declaration which can store 10-100 million
+    3. if we declare vector which is like a heap
+    so vector is best for declare ok 
+    which can store 10-100 million
+
+    */
+
+    return 0;
+}
