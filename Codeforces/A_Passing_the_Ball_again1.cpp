a527ccc21dffdeb8655f1792f2c71d71eb186f64
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-17 04:36:32 +0600

    A_Passing_the_Ball_again1.cpp

---
+++ Codeforces/A_Passing_the_Ball_again1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        for(int i=0; i<n; i++){
+            if(s[i]=='L'){
+                cout << i+1 << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
