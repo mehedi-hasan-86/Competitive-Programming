79af64f16ad4d946cd8830fd9af5899f7dc03ba1
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-17 06:30:53 +0600

    B_Right_Maximum_again1.cpp

---
+++ Codeforces/B_Right_Maximum_again1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n; cin >> n;
+
+        vi a(n);
+        for(int i=0; i<n; i++) cin >> a[i];
+
+        int mx = a[0];
+        int cnt = 0;
+        for(int i=0; i<n; i++){
+            if(mx<=a[i]){
+                cnt++;
+                mx = a[i];
+            }
+        }
+        cout << cnt << endl;
+    }
+
+    return 0;
+}
