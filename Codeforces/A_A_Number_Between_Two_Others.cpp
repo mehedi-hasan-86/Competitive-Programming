63b634fbca88b55d746d33b522bf16f5150a38f2
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-22 04:44:24 +0600

    A_A_Number_Between_Two_Others.cpp

---
+++ Codeforces/A_A_Number_Between_Two_Others.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        lli x,y;
+        cin >> x >> y;
+        if(y/x*(1.0)<=2){
+            cout <<"NO" << endl;
+        }else{
+            cout <<"YES" << endl;
+        }
+    }
+
+    return 0;
+}
