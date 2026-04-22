129f7f72bd24299bd68622943bb09de186c4999b
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-22 19:48:39 +0600

    Lec11.cpp

---
+++ Luv_CP/Lec11.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+void printHello(){
+    cout <<"Hello World" << endl;
+}
+
+int sum(int a, int b){
+    int res = a+b;
+    return res;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    printHello();
+    cout << sum(2,3) << endl;
+
+
+
+    return 0;
+}
