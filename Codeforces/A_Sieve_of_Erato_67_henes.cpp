3de20f368612692fce97d0ccabec2d4774253754
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-04-27 04:40:53 +0600

    A_Sieve_of_Erato_67_henes.cpp

--- Codeforces/A_Sieve_of_Erato_67_henes.cpp
+++ Codeforces/A_Sieve_of_Erato_67_henes.cpp
@@ -12,29 +12,21 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-
     int t;
     cin >> t;
 
     while(t--){
         int n;
-        cin >> n;
-        
+        cin>> n;
+
         vi a(n);
         for(int i=0; i<n; i++){
             cin >> a[i];
         }
-        bool isC = false;
-        for(int i=0; i<n; i++){
-            if(a[i]==67){
-                isC = true;
-            }
-        }
-        if(isC){
-            cout <<"YES" << endl;
-        }else{
-            cout <<"NO" << endl;
-        }
+
+        int cnt67 = count(a.begin(), a.end(), 67);
+        if(cnt67)  cout <<"YES" << endl;
+        else cout << "NO" << endl;
     }
 
     return 0;
