ec968a3ac7668d1205065b42849ede5f1c72318a
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-05-03 11:53:29 +0600

    A_Koshary.cpp

---
+++ Codeforces/A_Koshary.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int x,y;
+        cin >> x >> y;
+
+        if(x%2==1 && y%2==1){
+            cout <<"NO" << endl;
+        }else {
+            cout <<"YES" << endl;
+        }
+    }
+
+    return 0;
+}
