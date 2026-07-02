c1d938948e39c0f1a13d881805063ef0bc1beb0c
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-07-02 06:00:44 +0600

    A_Games_on_the_Train.cpp

---
+++ Codeforces/A_Games_on_the_Train.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vi h(n);
+        for(int i=0; i<n; i++){
+            cin >> h[i];
+        }
+        int mx = *max_element(h.begin(), h.end());
+        int mn = *min_element(h.begin(), h.end());
+        cout << mx-mn+1 << endl;
+        
+    }
+
+    return 0;
+}
