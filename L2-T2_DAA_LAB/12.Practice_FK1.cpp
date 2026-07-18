0dfb7c1c5ab6ae59fd76ad8f4bcd07b8485a7928
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-07-18 15:50:19 +0600

    12.Practice_FK1.cpp

---
+++ L2-T2_DAA_LAB/12.Practice_FK1.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+struct Item{
+    int val, wei;
+};
+
+bool cmp(Item a, Item b){
+    double r1 = (double) a.val/ a.wei;
+    double r2 = (double) b.val/b.wei;
+
+    return r1>r2;
+}
+
+void fk(vector<Item> &items, int W){
+    sort(items.begin(), items.end(), cmp);
+
+    double mxProfit = 0.0;
+    for(auto item : items){
+        if(W>= item.wei){
+            W -= item.wei;
+            mxProfit += item.val;
+        }else{
+            mxProfit += (double) item.val * W / item.wei;
+            break;
+        }
+    }
+    cout << mxProfit << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<Item> items(n);
+    for(int i=0; i<n; i++){
+        cin >> items[i].val >> items[i].wei;
+    }
+
+    int W;
+    cin >> W;
+
+    fk(items, W);
+
+    return 0;
+}
