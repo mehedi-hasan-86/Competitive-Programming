4e57665c1973ab88b7977c0e3c6a45f658a6d8e0
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-07-19 06:59:46 +0600

    A_Iskander_and_Drawings.cpp

---
+++ Codeforces/A_Iskander_and_Drawings.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int cnt = 0;
+        int mx = 0;
+        for(int i=0; i<n; i++){
+            if(s[i]=='#'){
+                cnt++;
+            }else{
+                mx = max(cnt, mx);
+                cnt = 0;
+            }
+        }
+        mx = max(cnt, mx);
+        cout << ceil(mx*1.0/2) << endl;
+    }
+
+    return 0;
+}
