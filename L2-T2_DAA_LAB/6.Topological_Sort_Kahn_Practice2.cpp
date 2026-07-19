2438cd20158e183492a89debdaeacd806720df50
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-07-19 11:19:29 +0600

    6.Topological_Sort_Kahn_Practice2.cpp

---
+++ L2-T2_DAA_LAB/6.Topological_Sort_Kahn_Practice2.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+void kahns(vector<vector<int>> &adj, int n){
+    vector<int> indegree(n,0);
+
+    for(int i=0; i<n; i++){
+        for(auto v : adj[i]){
+            indegree[v]++;
+        }
+    }
+    queue<int> q;
+    for(int i=0; i<n; i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+
+    vector<int> topoOrder;
+    while(!q.empty()){
+        int  x = q.front();
+        q.pop();
+        topoOrder.push_back(x);
+
+        for(auto v : adj[x]){
+            indegree[v]--;
+            if(indegree[v]==0){
+                q.push(v);
+            }
+        }
+    }
+    if(topoOrder.size() != n){
+        cout << "Graph has Cycle\n";
+        return;
+    }
+
+    for(int i=0; i<n; i++){
+        cout << topoOrder[i] << " ";
+    }
+    cout<< endl;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n,e;
+    cin >> n >> e;
+
+    vector<vector<int>> adj(n);
+    while(e--){
+        int u,v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+
+    kahns(adj, n);
+
+    return 0;
+}
