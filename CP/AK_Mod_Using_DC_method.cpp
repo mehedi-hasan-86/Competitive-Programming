c9915bf7767a96702ef1a06a9f2da9cdebe3958e
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-01 15:29:46 +0600

    AK_Mod_Using_DC_method.cpp

---
+++ CP/AK_Mod_Using_DC_method.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    return 0;
+}
