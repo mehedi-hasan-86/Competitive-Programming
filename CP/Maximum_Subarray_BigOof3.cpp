ff0ac089e27be2b823971d9d77958dff9ff3703c
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-08 16:35:47 +0600

    Maximum_Subarray_BigOof3.cpp

---
+++ CP/Maximum_Subarray_BigOof3.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    vi x(n);
+    for(int i=0; i<n; i++){
+        cin >> x[i];
+    }
+    int mxSum = 0;
+    for(int a=0; a<n; a++){
+        for(int b = a; b<n; b++){
+            int sum = 0;
+            for(int k=a; k<=b; k++){
+                sum += x[k];
+            }
+            mxSum = max(mxSum, sum);
+        }
+    }
+    cout << mxSum << endl;
+
+    return 0;
+}
