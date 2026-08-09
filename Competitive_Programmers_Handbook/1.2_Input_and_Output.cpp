4142df0c6bc3c5e727de2d3ded92f57a1f04dbbc
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-09 16:21:28 +0600

    1.2_Input_and_Output.cpp

---
+++ Competitive_Programmers_Handbook/1.2_Input_and_Output.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+/*
+Incudes all standard libraries at once
+No need for separate inluces
+ */
+using namespace std;
+/*
+No need to wirte std:: beore cin/cout
+*/
+
+int main(){
+    // Your solution comes here
+
+    int a,b;
+    string x;
+    cin  >> a >> b >> x;
+
+    cout << a << " " << b << " " << x<< endl;
+
+    return 0;
+}
