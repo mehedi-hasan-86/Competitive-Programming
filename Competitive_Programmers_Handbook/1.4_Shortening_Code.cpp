c3d767d4f4c585ba02fe20136ae6ad5f07c44ca2
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-09 16:58:51 +0600

    1.4_Shortening_Code.cpp

---
+++ Competitive_Programmers_Handbook/1.4_Shortening_Code.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define lli long long int
+#define endl "\n"
+#define sz(x) (int)(x).size()
+using vl = vector<lli>;
+using vi = vector<int>;
+const int MAX = 1e6 + 5;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // Basic typedef
+    typedef long long ll;
+
+    // Now you can use:
+    ll a = 1234789LL;
+    ll b = 984321LL;
+    cout << a * b << "\n";
+
+    // Complex typedefs
+    typedef vector<int> vi;
+    typedef pair<int,int> pi;
+
+    // Usage:
+    vi numbers;
+    pi p1 = {1, 2};
+
+    return 0;
+}
