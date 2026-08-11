082c2662f5e435d90af4a9cb7f1b8f2d24f69963
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-11 20:04:22 +0600

    7.1_Coin_Problem_Recursive_Without_memoization.cpp

---
+++ Competitive_Programmers_Handbook/7.1_Coin_Problem_Recursive_Without_memoization.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vll;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+
+vi coins = {3,1,4};
+int solve(int x){
+    if(x<0) return INF;
+    if(x==0) return 0;
+
+    int best = INF;
+    for(int c : coins){
+        best = min(best, solve(x-c)+1);
+    }
+    return best;
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n = 6;
+    cout << solve(n) << endl;
+
+    return 0;
+}
