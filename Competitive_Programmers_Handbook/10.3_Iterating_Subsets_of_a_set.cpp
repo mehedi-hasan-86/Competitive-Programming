f9ba15c29ff16dd38a13b214ee23be9814bce65a
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-13 16:27:04 +0600

    10.3_Iterating_Subsets_of_a_set.cpp

---
+++ Competitive_Programmers_Handbook/10.3_Iterating_Subsets_of_a_set.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vll;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    int x = 0;
+
+    for(int i=0; i<n; i++){
+        int ele;
+        cin >> ele;
+
+        x = x | (1<<ele);
+    }
+    int mask = x;
+        
+        do{
+            cout << "{";
+            for(int i=0; i<32; i++){
+                if(mask & (1<<i)) cout << i << " "; 
+            }
+            cout << "}\n";
+            mask = (mask-1)&x;
+
+        }while(mask != x);
+    
+
+    return 0;
+}
