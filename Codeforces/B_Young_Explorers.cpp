5b29dff0f02680b9499ce6a45de617b427a49e1e
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-13 10:57:31 +0600

    B_Young_Explorers.cpp

---
+++ Codeforces/B_Young_Explorers.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vll;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vi a(n);
+        for(int i=0; i<n; i++){
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+
+        int cnt = 0;
+        int grp = 0;
+        for(int i=0; i<n; i++){
+            cnt++;
+            if(cnt>=a[i]){
+                grp++;
+                cnt=0;
+            }
+        }
+        cout << grp << endl;
+    }
+
+    return 0;
+}
