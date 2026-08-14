991cb2a9e5642edccbb018db7cf137b3d188cc0e
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-14 20:59:33 +0600

    5.2_Permutation_of_String.cpp

---
+++ Competitive_Programmers_Handbook/5.2_Permutation_of_String.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vll;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s = "ABC";
+    sort(s.begin(),s.end());
+
+    do{
+        for(auto x : s){
+            cout << x << " ";
+        }
+        cout<< endl;
+
+    }while(next_permutation(s.begin(), s.end()));
+
+    return 0;
+}
