0ccbd93b260bd56bb0e86c094e19a98fdec6128d
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-14 20:55:23 +0600

    5.2_Generating_permutation_Next_permutation.cpp

---
+++ Competitive_Programmers_Handbook/5.2_Generating_permutation_Next_permutation.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vll;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n = 3;
+    vi prem;
+
+    for(int i=0; i<n; i++){
+        prem.push_back(i);
+    }
+
+    do{
+        for(int x : prem){
+            cout << x << " ";
+        }
+        cout << endl;
+        
+    }while(next_permutation(prem.begin(), prem.end()));
+
+    
+
+    return 0;
+}
