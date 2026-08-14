c7e084cee78ddab4e56b4402f2b5abf581d7210c
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-14 20:43:41 +0600

    5.2_Generating_Permutations_Recursive_Method.cpp

---
+++ Competitive_Programmers_Handbook/5.2_Generating_Permutations_Recursive_Method.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vll;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    
+
+    return 0;
+}
