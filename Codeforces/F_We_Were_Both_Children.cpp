7329c1a6723562fcb809b929ff483e6cae3b03ce
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-08-20 07:20:27 +0600

    F_We_Were_Both_Children.cpp

---
+++ Codeforces/F_We_Were_Both_Children.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vl;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+
+        vl a(n), cnt(n+1,0), mx(n+1,0);
+        for(int i=0; i<n; i++){
+            cin >> a[i];
+            if(a[i]<=n) cnt[a[i]]++;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=i; j<=n; j+=i){
+                mx[j] += cnt[i];
+            }
+        }
+
+        cout << *max_element(all(mx)) << endl;
+      
+    }
+
+    
+
+    return 0;
+}
