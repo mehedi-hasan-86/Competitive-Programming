037ed4063a7bf236132316fb9f3b7ac961cc5c25
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-09-04 19:54:34 +0600

    B_Good_times_Good_times.cpp

---
+++ Codeforces/B_Good_times_Good_times.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vl;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        lli x ;
+        cin >> x;
+
+        lli p = 1;
+
+        while(p<=x){
+            p *=10;
+        }
+        lli y = p+1;
+        cout << y << endl;
+    }
+
+    return 0;
+}
