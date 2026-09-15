57dff7576d43357b2c1e3fc68d28cc9b88ece481
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-09-15 08:30:52 +0600

    A_Creating_Abbreviations.cpp

---
+++ Codeforces/A_Creating_Abbreviations.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vl;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n,m;
+        cin >> n >> m;
+
+        vector<bool> av(26);
+        for(int i=0; i<n; i++){
+            string s;
+            cin >> s;
+           
+            av[s[0]-'a']  = true;
+        }
+        bool ok = true;
+        for(int i=0; i<m; i++){
+            string st;
+            cin >> st;
+
+            for(auto c : st){
+                if(!av[c-'A']){
+                    ok = false;
+                }
+            }
+        }
+        cout << (ok ? "YES":"NO") << endl;
+    }
+
+    return 0;
+}
