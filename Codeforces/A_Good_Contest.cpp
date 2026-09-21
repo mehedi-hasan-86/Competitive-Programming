5897c17b26ca602869bdfcbbf6fe88e8aaef5f0c
Mehedi Hasan <mehedi.hasan.cse86@gmail.com>
2026-09-22 05:03:17 +0600

    A_Good_Contest.cpp

---
+++ Codeforces/A_Good_Contest.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for (int i = a; i <= b; i++)
+#define all(x) (x).begin(), (x).end()
+#define endl '\n'
+typedef long long int lli;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+typedef vector<long long> vl;
+typedef vector<pair<int,int>> vpi;
+const int INF = 1e9;
+const int MOD = 1e9 + 7;
+const int N = 1e6;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+
+        vi a(3);
+        for(int i=0; i<3; i++){
+            cin >> a[i];
+        }
+        int ele = *min_element(a.begin(), a.end());
+        // cout << ele << endl;
+        cout << n-ele << endl;
+    }
+    return 0;
+}
